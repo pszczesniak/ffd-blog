2ffe87355fc26cb6d9b9a81cc183bfb8fff0a118
Piotr Szczęśniak <szczesniakp@o2.pl>
2021-06-08 21:57:33 +0200

    about page h1 style

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -20,3 +20,18 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  font: {
+    size: 6em;
+    weight: bold;
+  }
+  margin: 0;
+  line-height: 1;
+  background: linear-gradient(to left, var(--gradient-from-color), var(--gradient-to-color) 100%);
+  background-position: 0 100%;
+  background-size: 100% 20px;
+  background-repeat: repeat-x;
+}
+</style>
